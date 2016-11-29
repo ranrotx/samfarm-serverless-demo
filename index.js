bf48d800e67c39fa6f81b9a4b3f5c88ed002b2b3
Rory Jacob <roryj@amazon.com>
2016-11-29 00:08:29 -0800

    Update SAM count back down to 1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ exports.handler = (event, context, callback) => {
 
     emitLambdaAge();
 
-    var samCount = 17;
+    var samCount = 1;
     var samMultiplier = process.env.SAM_MULTIPLIER;
     var totalSAMs = samCount * samMultiplier;
 
